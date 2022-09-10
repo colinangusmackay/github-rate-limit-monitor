ac4144a64b7b5de293bb847893ea14ab35c616d4
Colin Mackay <github@colinmackay.scot>
2022-09-10 23:38:01 +0100

    Get the rate limits for the given token

--- index.js
+++ index.js
@@ -54,6 +54,44 @@ async function updateRateLimitInfo() {
 
     const data = await fetchRateData(pat, user);
     console.log(data);
+
+    const dateOptions = {dateStyle: "full", timeStyle:"full"}
+    document.getElementById("lastFetch").innerText = new Date().toLocaleString("en-gb", dateOptions);
+
+    let html = "";
+    const orderedResources = Object.entries(data.resources)
+        .sort((a,b)=>{
+            const aa = a[0];
+            const bb = b[0];
+            if (aa < bb)
+                return -1;
+            if (aa > bb)
+                return 1;
+            return 0;
+        });
+    const timeOptions = {hour12:false, hour:"2-digit", minute:"2-digit", second:"2-digit", timeZoneName:"short"};
+    for(const [name, values] of orderedResources){
+        console.log(`${name} => ${JSON.stringify(values)}`);
+        const friendlyName = name
+            .replace("_", " ")
+            .replace("_", " ");
+        const resetTime = new Date(values.reset * 1000).toLocaleTimeString("en-gb", timeOptions);
+        const percent = (values.used / values.remaining).toFixed(0);
+        html += `<tr><td rowspan='2' scope='row'>${friendlyName}</td>`;
+        html += `<td>${values.limit}</td>`;
+        html += `<td>${values.used}</td>`;
+        html += `<td>${values.remaining}</td>`;
+        html += `<td>${resetTime}</td>`;
+        html += "</tr>";
+        html += "<tr><td colspan='4'>";
+        html += "<div class='progress'>";
+        html += `<div class='progress-bar' role='progressbar' aria-label='Progress of ${friendlyName}' style='width: ${percent}%' aria-valuenow='${values.used}' aria-valuemin='0' aria-valuemax='${values.limit}'>${values.used} of ${values.limit}</div>`;
+        html += "</div>";
+        html += "</td></tr>"
+    }
+    document.getElementById("renderedData").innerHTML = html;
+    console.log(html);
+    document.getElementById("dataDisplay").style.display = "";
 }
 
 async function fetchRateData(pat, user){
